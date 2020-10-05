712519d617602935934e52ab5833d5fd7d2a99f6
zastrixarundell <zastrixarundell@gmail.com>
2020-10-05 16:19:34 +0200

    Updates test.exs

--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,12 @@
 use Mix.Config
 
+config :profile_place, ProfilePlace.Repo,
+  pool: Ecto.Adapters.SQL.Sandbox,
+  database: System.get_env("DATABASE_NAME", "profile_place_dev"),
+  username: System.get_env("DATABASE_USERNAME", "postgres"),
+  password: System.get_env("DATABASE_PASSWORD", "postgres"),
+  hostname: System.get_env("DATABASE_HOSTNAME", "localhost")
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :profile_place, ProfilePlaceWeb.Endpoint,
