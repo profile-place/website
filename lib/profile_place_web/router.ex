6560f7255174f36a89d0c9f191441ed89483ec9a
zastrixarundell <zastrixarundell@gmail.com>
2020-10-05 15:53:48 +0200

    Added LiveDashboard button

--- lib/profile_place_web/router.ex
+++ lib/profile_place_web/router.ex
@@ -26,4 +26,20 @@ defmodule ProfilePlaceWeb.Router do
   if Mix.env() == :dev do
     forward "/sent_emails", Bamboo.SentEmailViewerPlug
   end
+
+  # Enables LiveDashboard only for development
+  #
+  # If you want to use the LiveDashboard in production, you should put
+  # it behind authentication and allow only admins to access it.
+  # If your application does not have an admins-only section yet,
+  # you can use Plug.BasicAuth to set up some basic authentication
+  # as long as you are also using SSL (which you should anyway).
+  if Mix.env() in [:dev, :test] do
+    import Phoenix.LiveDashboard.Router
+
+    scope "/" do
+      pipe_through :browser
+      live_dashboard "/dashboard", metrics: ProfilePlaceWeb.Telemetry
+    end
+  end
 end
